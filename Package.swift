677a855cc88f896c6257f071595e502a30baffb1
consuelita <adelita@operatorfoundation.org>
2021-06-01 18:40:54 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,22 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "HKDF",
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v14)
+    ],
     products: [
         .library(name: "HKDF", targets: ["HKDF"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.14"),
+        .package(name: "Cryptor", url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.14"),
     ],
     targets: [
         .target(name: "HKDF", dependencies: ["Cryptor"], path: "Sources"),
         .testTarget(name: "HKDFTests", dependencies: ["HKDF"]),
     ],
-    swiftLanguageVersions: [4]
+    swiftLanguageVersions: [.v5]
 )
